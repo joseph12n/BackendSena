a4986b9bc02e3af7197d0e659cd56dde9f1e9bd6
Joseph Nicolas Varon Vargas <josephvaron45@gmail.com>
2026-02-09 20:06:44 -0500

    DB carpetas

---
+++ backend/config/auth.config.js
@@ -0,0 +1,15 @@
+//carga de variables de entorno desde .env 
+require('dotenv').config();
+
+module.exports= {
+    //clave para firmar los tokens de jwt
+    secret: process.new.JWT_SECRET ||
+    "asdola",
+    //tiempo de expiracion del token en segundos
+    jwtExpiration: process.env.JWT_EXPIRATION ||
+    86400, //24 HORAS
+    //Tiempo de expiracion de refrescar el token
+    jwtRefresh: 6048000, // 7 Dias
+    //Numero de rondas para encriptar la contraseña
+    slatRounds: process.env.SALT_ROUNDS || 8
+};
---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+//conexion BD
+module.exports = {
+    url:process.env.MONGODB_URI ||
+    "mongodb://localhost:27017/crud-mongo"
+};
