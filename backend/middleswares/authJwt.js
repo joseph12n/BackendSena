a70ba319e50f95bc1af6b2dd258e3cfeb2027def
Joseph Nicolas Varon Vargas <josephvaron45@gmail.com>
2026-02-25 21:15:12 -0500

    Middleware JWT role
    se crea y concluye el middleware de JWT y se inicia el de rol

---
+++ backend/middleswares/authJwt.js
@@ -0,0 +1,76 @@
+/**
+ * MIDDLEWARE DE VERIFICACION JWT
+ * middleware para verificar y validar tokens JWT en las solicitudes
+ * se usa en todas las rutas protegidas para autenticar usuarios
+ * caracteristicas:
+ * soporta dos formatos
+ * 1 authoriztion: Bearer <token> (Estandar REST)
+ * 2 x-access-token (token personalizado)
+ * extrae informacion del tojen (id role email)
+ * la adjunta a req.userId req.userRole, req.userEmail para uso en los controladores
+ * manejo de errores con codigos 403/401 apropiados
+ * flujo
+ * 1. lee el header Authorization o x-access-token
+ * 2. Extrae el token (quita el Bearer si es necesario)
+ * 3. Verifica el token con JWT_SECRET
+ * 4. si es valido continua al siguiente middleware
+ * 5. si es invalido retorna 401 Unauthorized
+ * 6. si falta retorna 403 Forbidden
+ * 
+ * validacion del token 
+ * 1. Verifica firma criptografica con JWT_SECRET
+ * 2. Comprueba que no haya expirado
+ * 3. Extrae payload {id, role, email}
+ */
+
+const jwt = require('jsonwebtoken');
+const config = require('../config/auth.config');
+
+/**
+ * Verificar token
+ * Funcionalidad
+ * busca el token en las ubicaciones posibles (orden de procedencia)
+ * 1. header Authorization con formato Bearer <token>
+ * 2. header x-access-token
+ * si encuentra el token verifica su validez 
+ * si no encuentra retorna 403: Forbidden
+ * si el token es invalido / expirado retorna 401: Unauthorized
+ * si es valido adjunta datos del usuario a req y contina
+ * 
+ * Headers soportados:
+ * 1. Authorization bearer <kgdsafhgksjf>
+ * 2. x-access-token: <fghkdsajgfha..>id, role, email
+ * propiedades del request despues del middleware:
+ * req.userId = (string) Id del usuario MongoDB
+ * req.userRole = (string) rol del usuario (admin, coordinador, auxiliar)
+ * req.userEmail = (string) email del usuario  
+ */
+
+const verifyTokenFn = (req, res, next) => {
+    try{
+        //soportar dos formatos de Authorization bearer o access-token
+        let token = null;
+
+        //formato Authorization
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+            //Extraer token quitando el Bearer 
+            token = req.headers.authorization.substring(7); 
+        }
+
+        // formato access-token
+        else if (req.headers['x-access-token']){
+            token = req.headers['x-access-token'];
+        }
+        
+        //si no encontro token rechaza la solicitud
+        if(!token){
+            return res.status(403).json({
+                success: false,
+                message: 'Token no proporcionado'
+            });
+        }
+
+        //verificar el token con la clave secreta
+        const decoded = jwt.verify(token, config.secret);
+    }
+}
