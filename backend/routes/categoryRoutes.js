a00e1ca63075c43424a11bf09adcfb509c41707f
Joseph Nicolas Varon Vargas <josephvaron45@gmail.com>
2026-02-27 21:12:50 -0500

    rutas creadas y concluidas

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,42 @@
+/**
+ * Rutas de categororias
+ * define los endpoints CRUD para la gestion de categorias
+ * las categorias son contenedores padres de subcategorias y productos
+ * endpoints:
+ * Post /api/categories crea una nueva categoria
+ * Get /api/categories obtiene todas las categorias
+ * Get /api/categories/:id obtiene una categoria por id
+ * Put /api/categories/:id actualiza una categoria por id
+ * Delete /api/categories/:id elimina una categoria/desactivar 
+ */
+
+const express = require('express');
+const router = express.router();
+const categoryController = require('../controllers/categoryController');
+const { verifyToken } = require('../middleswares/authJwt');
+const { checkRole } = require('../middleswares/role');
+
+//Rutas CRUD
+
+router.post('/',
+    verifyToken,
+    checkRole(['admin','coordinador']),
+    categoryController.createCategory
+);
+
+router.get('/', categoryController.getCategories);
+
+router.get('/:id', categoryController.getCategoryById);
+
+router.put('/:id',
+    verifyToken,
+    checkRole(['admin','coordinador']),
+    categoryController.updateCategory
+);
+router.delete('/:id',
+    verifyToken,
+    checkRole(['admin']),
+    categoryController.deleteCategory
+);
+
+module.exports = router;
